ba33c6f4172a101488bac8fd54596727e51588d3
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-10-10 23:14:51 +0200

    fHexDump.fs: added start of command line arguments processing

--- Langages/ML/code/fHexDump/fHexDump/fHexDump/fHexDump.fs
+++ Langages/ML/code/fHexDump/fHexDump/fHexDump/fHexDump.fs
@@ -1,13 +1,43 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+(*
+    fHexDump.fs
 
-open System
+    bernard.tatin@outlook.fr
+    2021-10-10
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+    Hexdump written in F#
+ *)
 
-[<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+namespace fHexDump
+
+module main =
+    open System
+
+    let exe_name = "fHexDump"
+    let exe_version = "0.1.0"
+    // Define a function to construct a message to print
+    let help exit_code =
+        printf "%s - Some help from your friends\n" exe_name
+        printf "%s [OPTIONS] [FILES ...\n" exe_name
+        printf "%s [-h|--help]: show this text and exits\n" exe_name
+        printf "%s -v|--version: show the version informations then exits\n" exe_name
+        exit_code
+
+    let version () =
+        printf "%s - version %s\n" exe_name exe_version
+        0
+
+    let has_argument name short_name args =
+        args |> Array.exists (fun x -> x = ("--" + name) || x = ("/" + name) || x = sprintf "-%s" short_name)
+    let strip_argument name short_name args =
+        args |> Array.filter (fun x -> x <> ("--" + name) && x <> ("/" + name) && x <> sprintf "-%s" short_name)
+
+    [<EntryPoint>]
+    let main argv =
+        if (has_argument "help" "h" argv) then
+            let doit () = help 0
+            doit ()
+        else if (has_argument "version" "v" argv) then
+                 version ()
+        else
+            let doit () = help 1
+            doit ()
