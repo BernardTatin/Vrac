7eab4f043ea2c83b60d035495fac18e9fb606d40
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-10-11 00:36:57 +0200

    fHexDump.fs: just before a good command line arguments processing

--- Langages/ML/code/fHexDump/fHexDump/fHexDump/fHexDump.fs
+++ Langages/ML/code/fHexDump/fHexDump/fHexDump/fHexDump.fs
@@ -25,18 +25,34 @@ module main =
     let version () =
         printf "%s - version %s\n" exe_name exe_version
         0
+    let on_file f =
+        printf "File %s\n" f
+        0
+
+    let rec allfiles = function
+        | [] -> 0
+        | f :: rest -> on_file f;
+                        allfiles rest
 
     let has_argument name short_name args =
         args |> Array.exists (fun x -> x = ("--" + name) || x = ("/" + name) || x = sprintf "-%s" short_name)
     let strip_argument name short_name args =
-        args |> Array.filter (fun x -> x <> ("--" + name) && x <> ("/" + name) && x <> sprintf "-%s" short_name)
+        args |> Array.filter (fun x -> x <> ("--" + name)
+                                           && x <> ("/" + name)
+                                           && x <> sprintf "-%s" short_name)
+
 
     [<EntryPoint>]
     let main argv =
         if (has_argument "help" "h" argv) then
             help 0
-        if (has_argument "version" "v" argv) then
+        else if (has_argument "version" "v" argv) then
             version ()
         else
-            let doit () = help 1
-            doit ()
+            argv |> Array.toList |> allfiles
+(*
+            strip_argument "help" "h" argv
+//                |> strip_argument "version" "v" argv
+                |> Array.toList
+                |> allfiles
+*)
