a9f180a36aa44e6dcd54e74b1e706d1381eb2ca6
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-10-20 14:16:15 +0200

    IntegerGames: a first good version, more work needed

--- Langages/ML/code/IntegerGames/IntegerGames/Program.fs
+++ Langages/ML/code/IntegerGames/IntegerGames/Program.fs
@@ -16,7 +16,7 @@ module main =
     open Libraries.LibInt
 
     let help exit_code =
-        let lines = [ "integer-games command [OPTIONS] [PARAMETERS]";
+        let lines = [ "IntegerGames command [OPTIONS] [PARAMETERS]";
                       "command = ";
                       "    help : show this text and exit";
                       "    version : show version and exit";
@@ -24,10 +24,26 @@ module main =
                        ]
         print_lines lines exit_code
 
+    let version() =
+        let lines = ["IntegerGames version 0.1.0"]
+        print_lines lines 0
 
+    let fact arg =
+        let n = arg  |> int
+        printfn "%d! = %d" n (factorial n)
+
+    let all_facts = function
+        | [] -> help 1
+        | args -> (foreach fact args
+                   0)
+
+    let do_command = function
+        | [] -> help 0
+        | "help" :: _ -> help 0
+        | "version" :: _ -> version()
+        | "fact" :: tl -> all_facts tl
+        | _ -> help 1
 
     [<EntryPoint>]
     let main argv =
-        if argv.Length = 0 then help 0
-        else (printfn "%d! = %d" 10 (factorial 10);
-                0) // return an integer exit code
+        do_command (Array.toList argv)
