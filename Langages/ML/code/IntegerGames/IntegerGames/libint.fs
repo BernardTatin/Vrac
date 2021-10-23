c02e5a8fa3316e608f78213091af2a0c4e4e2eac
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-10-23 16:15:02 +0200

    IntegerGames: safe_add and safe_mul send error when there is an overflow

--- Langages/ML/code/IntegerGames/IntegerGames/libint.fs
+++ Langages/ML/code/IntegerGames/IntegerGames/libint.fs
@@ -11,24 +11,39 @@ module LibInt =
     open System
     open Libraries.LibTool
 
+    let safe_add x y =
+        let bad = (x > (System.Int32.MaxValue - y))
+        if bad then
+            on_error (sprintf "overflow on %d + %d" x y)
+            0
+        else
+            x + y
+
+    let safe_mul x y =
+        let bad = (x > (System.Int32.MaxValue / y))
+        if bad then
+            on_error (sprintf "overflow on %d * %d" x y)
+            0
+        else
+            x * y
+
     let factorial n =
         let rec inner_fact acc = function
                 | 0 | 1 -> acc
-                | k -> inner_fact (k * acc ) (k - 1)
+                | k -> inner_fact (safe_mul k acc) (k - 1)
         inner_fact 1 n
 
     let fibonacchi n =
         let rec inner_fibo = function
             | (0, a, _) -> a
             | (1, _, b) -> b
-            | (k, a, b) -> inner_fibo ((k - 1), b, (a + b))
+            | (k, a, b) -> inner_fibo ((k - 1), b, (safe_add a b))
         inner_fibo (n, 0, 1)
 
     let str2int (str: string) =
         let mutable result = 0
         if Int32.TryParse(str, &result) then
-            let rr = result
-            rr
+            result
         else
             on_error (sprintf "Unable to transform '%s' as an integer" str) |> ignore
             0
