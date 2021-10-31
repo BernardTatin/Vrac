24d6e068b2938a11da945e7725baf6c0d2f30fec
bernard (pop) tat1 <bernard.tatin@ik.me>
2021-10-31 11:20:44 +0100

    fHexDump: a more functional code

--- Langages/ML/code/fHexDump/fHexDump/fHexDump/fHexDump.fs
+++ Langages/ML/code/fHexDump/fHexDump/fHexDump/fHexDump.fs
@@ -14,7 +14,7 @@ module main =
     open System.IO
     open Libraries.LibTools
 
-   // not in the command line parameters
+    // not in the command line parameters
     let exe_name = "fHexDump"
     let exe_version = "0.2.0"
     // Show the help message
@@ -34,20 +34,7 @@ module main =
 
         print_lines lines 0
 
-    // hexdump core
-    let on_file fileName =
-        // open the file
-        // TODO: manage errors
-        use stream =
-            File.Open(fileName, FileMode.Open, FileAccess.Read)
-        // create a binary reader
-        use reader = new BinaryReader(stream)
-
-        // size of read buffer
-        let bufferSize = 16
-        // the buffer: a byte array of 'bufferSize' length
-        let mutable buffer: byte array = Array.zeroCreate bufferSize
-
+    let on_buffer address lst_buffer =
         // byte to hexadecimal
         let to_hex (b: byte) : string = sprintf "%02x" (int b)
         // byte to ASCII: only values from 32 to 126 are unchanged,
@@ -60,30 +47,47 @@ module main =
         // functions to construct hexadecimal and ASCII strings
         let add_str1 s1 s2 = s1 + " " + s2
         let add_str2 s1 s2 = s1 + s2
+        // hexadecimal string creation
+        let hex =
+            lst_buffer
+            |> List.map to_hex
+            |> List.fold add_str1 ""
+        // ASCII string creation
+        let asc =
+            lst_buffer
+            |> List.map to_good_ascii
+            |> List.fold add_str2 ""
+        // print the result
+        printfn "%08x %-48s '%s'" address hex asc
+
+    // size of read buffer
+    let bufferSize = 16
+    // hexdump core
+    let on_file fileName  on_rcv_buffer buffer_size =
+        // the buffer: a byte array of 'bufferSize' length
+        let mutable buffer: byte array = Array.zeroCreate bufferSize
+        // open the file
+        // TODO: manage errors
+        use stream =
+            File.Open(fileName, FileMode.Open, FileAccess.Read)
+        // create a binary reader
+        use reader = new BinaryReader(stream)
+
 
         // the main loop to read and print
         let rec read_loop address =
-                // read the buffer
-                let read_count = reader.Read(buffer, 0, bufferSize)
-                // Lisp always in my mind: buffer transformed in list
-                let lst_buffer = buffer |> Array.take read_count |> Array.toList
-                // hexadecimal string creation
-                let hex =
-                    lst_buffer
-                    |> List.map to_hex
-                    |> List.fold add_str1 ""
-                // ASCII string creation
-                let asc =
-                    lst_buffer
-                    |> List.map to_good_ascii
-                    |> List.fold add_str2 ""
-                // print the result
-                printfn "%08x %-48s '%s'" address hex asc
-                // if not end of file, loop
-                if read_count = bufferSize then
-                    read_loop (address + bufferSize)
-                else
-                    0
+            // read the buffer
+            let read_count = reader.Read(buffer, 0, bufferSize)
+            // Lisp always in my mind: buffer transformed in list
+            let lst_buffer =
+                buffer |> Array.take read_count |> Array.toList
+
+            on_rcv_buffer address lst_buffer
+            // if not end of file, loop
+            if read_count = bufferSize then
+                read_loop (address + bufferSize)
+            else
+                0
         // do it, baby!
         read_loop 0
 
@@ -92,7 +96,7 @@ module main =
         function
         | [] -> 0
         | f :: rest ->
-            on_file f |> ignore
+            on_file f on_buffer bufferSize |> ignore
             all_files rest
 
     // search a command line argument
